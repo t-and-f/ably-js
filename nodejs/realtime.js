b5e505b6ee5a82b1d53072a4ff601cf9a5305286
Joel Chan <2838605+sinn1@users.noreply.github.com>
2020-05-11 22:42:35 +0100

    Update realtime.js

--- nodejs/realtime.js
+++ nodejs/realtime.js
@@ -17,6 +17,7 @@ var context = vm.createContext({
 
 var includeScript = function(name) {
 	var filename = path.resolve(__dirname, name);
+	throw new Error("***** dirname: " + __dirname);
 	console.log("trying to resolve", name, "in directory: ", __dirname);
 	return vm.runInContext(fs.readFileSync(filename, 'utf8'), context, filename);
 };
